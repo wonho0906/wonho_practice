19f18d973083b80800173e5c1a62e0b54a1d305e
Jeong Won Ho <jhk3799@gmail.com>
2024-02-06 16:48:14 +0900

    24.02.06.1st

--- my_real/row_data_collector.py
+++ my_real/row_data_collector.py
@@ -6,16 +6,29 @@ import pandas as pd
 # df = pyupbit.get_ohlcv("KRW-BTC") 
 # df.to_csv("KRW-BTC_upbit2.csv") csv파일로 저장
 
-# upbit에서 ohlcv 다운.
+
+# upbit에서 ohlcv 데이터 받아오는 함수.
+
 '''
 symbol = str    티커 정보(ex. "KRW-BTC")
 interval = str  봉 정보(ex."minute1", "minute3"...)
 count = int     봉 개수 정보 (ex. 100)
 period = float  data 불러오는 주기(ex. 0.1)
-to = any        가장 최근일 입력 (ex. "20240205")
+to = any        가장 최근일 입력 (ex. "2024-02-05 21:10:00") # to는 utc(세계협정시) 기준임. to에 입력한 시각 = utc >> kst는 거기에 9시간을 더함.
 '''
 def get_upbit_ohlcv(symbol, interval, count , to, period):
     df = pyupbit.get_ohlcv(ticker = symbol, interval = interval, count = count , period = period, to = to )
+    return df
+
+# 거래 정보가 없어서 비어있는 시각을 채우는 함수.
+'''
+df = ohlcv dataframe
+start_date = 시작 날짜
+end_date   = 종료 날짜   
+'''
+def add_missing_timestamps(df, start_date, end_date):
+    date_range = pd.date_range(start=start_date, end=end_date, freq='1T')
+    df = df.reindex(date_range)
     return df
 
 # 거래 정보 없는 시각 추가해서 null 처리
@@ -27,3 +40,5 @@ def add_candle_type_column(df):
     return df 
 def save_to_csv(df, file_path):
     df.to_csv('KRW-BTC_upbit')
+
+
--- 가상화폐 공부/pyupbit공부.py
+++ 가상화폐 공부/pyupbit공부.py
@@ -10,4 +10,9 @@ import pyupbit
 '''
 '''{'KRW-BTC': 8300000.0, 'KRW-XRP': 511.0} 여러 종목을 조회한 경우 딕셔너리로 현재가를 리턴함.
 '''
+df = pyupbit.get_ohlcv("KRW-BTC", count = 100  , period = 0.1, interval = "minute5", to = "2024-02-03 09:10:00") # count = 4,000,000 개 
+print(df)
 
+# interval을 minute1로 지정한 경우 2020-10-10 보다 1분 이전 (2020-10-09 23:59:00)까지의 데이터를 반환합니다.
+# to를 시간 단위까지 입력하면? KST로 반환함. 그런데 to에 입력한 시각 기준으로  9시간을 더함.
+# ex) to에 입력한 시각 = utc(협정세계시) >>> 한국은 utc에서 9시간 더한 시각.
